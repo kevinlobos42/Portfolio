e101a70aec9974da89d6366de34fab8314059a94
kevinlobos42 <kevinlobos42@gmail.com>
2021-03-11 22:18:31 -0500

    added 2 ReactJs projects

--- js/box-scroll.js
+++ js/box-scroll.js
@@ -10,14 +10,13 @@ const html_5 = document.getElementById("html-proj-5");
 const react_1 = document.getElementById("react-proj-1");
 const react_2 = document.getElementById("react-proj-2");
 const react_3 = document.getElementById("react-proj-3");
-const react_4 = document.getElementById("react-proj-4");
-const react_5 = document.getElementById("react-proj-5");
 
-const vue_1 = document.getElementById("vue-proj-1");
-const vue_2 = document.getElementById("vue-proj-2");
-const vue_3 = document.getElementById("vue-proj-3");
-const vue_4 = document.getElementById("vue-proj-4");
-const vue_5 = document.getElementById("vue-proj-5");
+
+// const vue_1 = document.getElementById("vue-proj-1");
+// const vue_2 = document.getElementById("vue-proj-2");
+// const vue_3 = document.getElementById("vue-proj-3");
+// const vue_4 = document.getElementById("vue-proj-4");
+// const vue_5 = document.getElementById("vue-proj-5");
 
 const html_dot_1 = document.getElementById("html-dot-1");
 const html_dot_2 = document.getElementById("html-dot-2");
@@ -28,18 +27,17 @@ const html_dot_5 = document.getElementById("html-dot-5");
 const react_dot_1 = document.getElementById("react-dot-1");
 const react_dot_2 = document.getElementById("react-dot-2");
 const react_dot_3 = document.getElementById("react-dot-3");
-const react_dot_4 = document.getElementById("react-dot-4");
-const react_dot_5 = document.getElementById("react-dot-5");
 
-const vue_dot_1 = document.getElementById("vue-dot-1");
-const vue_dot_2 = document.getElementById("vue-dot-2");
-const vue_dot_3 = document.getElementById("vue-dot-3");
-const vue_dot_4 = document.getElementById("vue-dot-4");
-const vue_dot_5 = document.getElementById("vue-dot-5");
+
+// const vue_dot_1 = document.getElementById("vue-dot-1");
+// const vue_dot_2 = document.getElementById("vue-dot-2");
+// const vue_dot_3 = document.getElementById("vue-dot-3");
+// const vue_dot_4 = document.getElementById("vue-dot-4");
+// const vue_dot_5 = document.getElementById("vue-dot-5");
 
 html_dots = [html_dot_1, html_dot_2, html_dot_3, html_dot_4, html_dot_5];
-react_dots = [react_dot_1, react_dot_2, react_dot_3, react_dot_4, react_dot_5];
-vue_dots = [vue_dot_1, vue_dot_2, vue_dot_3, vue_dot_4, vue_dot_5];
+react_dots = [react_dot_1, react_dot_2, react_dot_3];
+// vue_dots = [vue_dot_1, vue_dot_2, vue_dot_3, vue_dot_4, vue_dot_5];
 
 html_dots.forEach(dot => {
   dot.addEventListener("click", htmlScroll);
@@ -47,9 +45,9 @@ html_dots.forEach(dot => {
 react_dots.forEach(dot => {
   dot.addEventListener("click", reactScroll);
 });
-vue_dots.forEach(dot => {
-  dot.addEventListener("click", vueScroll);
-});
+// vue_dots.forEach(dot => {
+//   dot.addEventListener("click", vueScroll);
+// });
 function htmlScroll(e) {
   let target = e.target;
   console.log(target);
@@ -94,12 +92,6 @@ function reactScroll(e) {
     case react_dot_3:
       _left = react_3.offsetLeft;
       break;
-    case react_dot_4:
-      _left = react_4.offsetLeft;
-      break;
-    case react_dot_5:
-      _left = react_5.offsetLeft;
-      break;
   }
   react.scroll({
     top: 0,
@@ -107,31 +99,31 @@ function reactScroll(e) {
     behavior: "smooth"
   });
 }
-function vueScroll(e) {
-  let target = e.target;
-  console.log(target);
-  vue_dots.forEach(dot => dot.classList.remove("selected"));
-  target.classList.add("selected");
-  switch (target) {
-    case vue_dot_1:
-      _left = vue_1.offsetLeft;
-      break;
-    case vue_dot_2:
-      _left = vue_2.offsetLeft;
-      break;
-    case vue_dot_3:
-      _left = vue_3.offsetLeft;
-      break;
-    case vue_dot_4:
-      _left = vue_4.offsetLeft;
-      break;
-    case vue_dot_5:
-      _left = vue_5.offsetLeft;
-      break;
-  }
-  vue.scroll({
-    top: 0,
-    left: _left - vue_1.offsetLeft,
-    behavior: "smooth"
-  });
-}
+// function vueScroll(e) {
+//   let target = e.target;
+//   console.log(target);
+//   vue_dots.forEach(dot => dot.classList.remove("selected"));
+//   target.classList.add("selected");
+//   switch (target) {
+//     case vue_dot_1:
+//       _left = vue_1.offsetLeft;
+//       break;
+//     case vue_dot_2:
+//       _left = vue_2.offsetLeft;
+//       break;
+//     case vue_dot_3:
+//       _left = vue_3.offsetLeft;
+//       break;
+//     case vue_dot_4:
+//       _left = vue_4.offsetLeft;
+//       break;
+//     case vue_dot_5:
+//       _left = vue_5.offsetLeft;
+//       break;
+//   }
+//   vue.scroll({
+//     top: 0,
+//     left: _left - vue_1.offsetLeft,
+//     behavior: "smooth"
+//   });
+// }
--- js/pg2-menu.js
+++ js/pg2-menu.js
@@ -1,22 +1,22 @@
 let js = document.getElementById("js-option");
 let react_opt= document.getElementById("react-option");
-let vue_opt= document.getElementById("vue-option");
+// let vue_opt= document.getElementById("vue-option");
 // let java= document.getElementById("java-option");
-let btns=[js,react_opt, vue_opt];
+let btns=[js,react_opt/*, vue_opt*/];
 let js_projs = document.getElementById("reg-projs");
 let react_projs = document.getElementById("react-projs");
 let vue_projs = document.getElementById("vue-projs");
 // let java_projs = document.getElementById("js-option");
-let arr = [js_projs, react_projs, vue_projs];
+let arr = [js_projs, react_projs/*, vue_projs*/];
 js.addEventListener('click', function(){
     remove(js_projs, js);
 });
 react_opt.addEventListener('click', function(){
     remove(react_projs, react_opt);
 });
-vue_opt.addEventListener('click', function(){
-    remove(vue_projs, vue_opt);
-});
+// vue_opt.addEventListener('click', function(){
+//     remove(vue_projs, vue_opt);
+// });
 
 function remove(opt, btn){
    arr.forEach(e=>{
